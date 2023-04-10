09acbcb7a62cec56c899233bf7b53d16d8857797
guaugua <128066589+guaugua@users.noreply.github.com>
2023-04-10 20:21:01 +0900

    Update volume_calc.py
    요금기능 추가

--- volume_calc.py
+++ volume_calc.py
@@ -9,3 +9,25 @@ height=float(height)
 volume=length*width*height
 
 print("박스의 부피는",volume,"입니다.")
+
+#택배요금
+total_length = length + width + height
+
+#80cm 이하 : 5000원
+#80~100cm : 8000원
+#100~120cm : 10000원
+#120~160cm : 13000원
+#160cm~ : 배송X
+
+if total_length <= 80:
+    charge = 5000
+elif total_length <= 100:
+    charge = 8000
+elif total_length <= 120:
+    charge = 10000
+elif total_length <= 160:
+    charge = 13000
+else:
+    charge = "nono"
+print("Total length:",total_length)
+print("Charge:",charge)
